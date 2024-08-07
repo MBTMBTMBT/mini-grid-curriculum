73dc4d0629f3d5cb7408dde6fc82e2484d4f5f97
Benteng Ma <1376937399@qq.com>
2024-08-07 03:47:30 +0100

    Add complete curriculum training framework
    Implemented the full framework for curriculum-based training in MiniGrid. This includes task and target configuration classes, evaluation callbacks, and the main CurriculumRunner class. Enhancements include model evaluation, logging, and saving during training.

--- minigrid_curriculum.py
+++ minigrid_curriculum.py
@@ -3,11 +3,12 @@ import numpy as np
 from typing import Dict, List, Union
 
 from gymnasium import Env
+from stable_baselines3 import PPO
 from stable_baselines3.common.type_aliases import PolicyPredictor
 from torch.utils.tensorboard import SummaryWriter
 from customize_minigrid.custom_env import CustomEnv
 from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor, VecEnv
-from stable_baselines3.common.callbacks import EventCallback
+from stable_baselines3.common.callbacks import EventCallback, CallbackList
 from stable_baselines3.common.evaluation import evaluate_policy
 import gymnasium as gym
 
@@ -48,7 +49,6 @@ class TargetConfig:
         self.random_rotate: bool = False
         self.random_flip: bool = False
         self.max_steps: int = 0
-        self.eval_episodes: int = 0
 
 
 def eval_envs(
@@ -87,6 +87,51 @@ def eval_envs(
     return np.mean(rewards)
 
 
+class EvalCallback(EventCallback):
+    def __init__(
+            self,
+            eval_envs: List[VecEnv],
+            eval_env_names: List[str],
+            log_writer: SummaryWriter,
+            eval_freq: int,
+            n_eval_episodes: int,
+            deterministic: bool = True,
+            verbose: int = 1,
+            start_timestep: int = 0,
+    ):
+        super().__init__(verbose=verbose)
+        self.eval_envs = eval_envs
+        self.eval_env_names = eval_env_names
+        self.eval_freq = eval_freq
+        self.n_eval_episodes = n_eval_episodes
+        self.deterministic = deterministic
+        self.log_writer = log_writer
+        self.evaluations: List[float] = []
+        self.start_timestep = start_timestep
+
+    def _on_step(self) -> bool:
+        # Evaluate the model at specified frequency
+        if self.n_calls % self.eval_freq == 0:
+            self.eval()
+        return True
+
+    def _on_training_end(self) -> None:
+        self.eval()
+
+    def eval(self):
+        print("Evaluating model over targets...")
+        mean_reward = eval_envs(
+            envs=self.eval_envs,
+            env_names=self.eval_env_names,
+            model=self.model,
+            num_eval_episodes=self.n_eval_episodes,
+            deterministic=self.deterministic,
+            log_writer=self.log_writer,
+            num_timesteps=self.num_timesteps + self.start_timestep,
+            verbose=self.verbose,
+        )
+
+
 class EvalSaveCallback(EventCallback):
     def __init__(
             self,
@@ -99,6 +144,7 @@ class EvalSaveCallback(EventCallback):
             n_eval_episodes: int,
             deterministic: bool = True,
             verbose: int = 1,
+            start_timestep: int = 0,
     ):
         super().__init__(verbose=verbose)
         self.eval_envs = eval_envs
@@ -111,35 +157,41 @@ class EvalSaveCallback(EventCallback):
         self.model_save_name = model_save_name
         self.log_writer = log_writer
         self.evaluations: List[float] = []
+        self.start_timestep = start_timestep
 
     def _on_step(self) -> bool:
         # Evaluate the model at specified frequency
         if self.n_calls % self.eval_freq == 0:
-            print("Evaluating model...")
-            mean_reward = eval_envs(
-                envs=self.eval_envs,
-                env_names=self.eval_env_names,
-                model=self.model,
-                num_eval_episodes=self.n_eval_episodes,
-                deterministic=self.deterministic,
-                log_writer=self.log_writer,
-                num_timesteps=self.num_timesteps,
-                verbose=self.verbose,
-            )
-
-            latest_path = os.path.join(self.model_save_dir, f"{self.model_save_name}_latest.zip")
-            self.model.save(latest_path)
-            if self.verbose >= 1:
-                print(f"Saved latest model to {latest_path}")
-
-            best_path = os.path.join(self.model_save_dir, f"{self.model_save_name}_best.zip")
-            if mean_reward > self.best_mean_reward:
-                self.best_mean_reward = mean_reward
-                self.model.save(best_path)
-                if self.verbose >= 1:
-                    print(f"New best model with mean reward {mean_reward:.2f} saved to {best_path}")
-
+            self.eval()
         return True
+
+    def _on_training_end(self) -> None:
+        self.eval()
+
+    def eval(self):
+        print("Evaluating model...")
+        mean_reward = eval_envs(
+            envs=self.eval_envs,
+            env_names=self.eval_env_names,
+            model=self.model,
+            num_eval_episodes=self.n_eval_episodes,
+            deterministic=self.deterministic,
+            log_writer=self.log_writer,
+            num_timesteps=self.num_timesteps + self.start_timestep,
+            verbose=self.verbose,
+        )
+
+        latest_path = os.path.join(self.model_save_dir, f"{self.model_save_name}_latest.zip")
+        self.model.save(latest_path)
+        if self.verbose >= 1:
+            print(f"Saved latest model to {latest_path}")
+
+        best_path = os.path.join(self.model_save_dir, f"{self.model_save_name}_best.zip")
+        if mean_reward > self.best_mean_reward:
+            self.best_mean_reward = mean_reward
+            self.model.save(best_path)
+            if self.verbose >= 1:
+                print(f"New best model with mean reward {mean_reward:.2f} saved to {best_path}")
 
 
 class CurriculumRunner:
@@ -164,12 +216,17 @@ class CurriculumRunner:
     def train(
             self,
             session_dir: str,
+            eval_freq: int,
+            num_eval_episodes: int,
+            eval_deterministic: bool,
             force_sequential: bool = False,
+            start_time_step: int = 0,
     ):
         """
         Trainer function for mini-grid curriculum
         Args:
             session_dir: directory to save the trained model and logs
+            eval_freq: frequency of evaluation steps
             force_sequential: if True, make all tasks sequentially even with same difficulty level
 
         Returns:
@@ -178,9 +235,11 @@ class CurriculumRunner:
         # prepare environments
         train_env_list = []
         train_env_step_list = []
-        train_env_name_list = []
+        # train_env_name_list = []
         eval_env_list = []
         eval_env_name_list = []
+        target_env_list = []
+        target_env_name_list = []
 
         # iterate through task dict to make sequence of environments.
         for difficulty_level in sorted(self.task_dict.keys()):
@@ -217,22 +276,80 @@ class CurriculumRunner:
                     train_env_list.append(VecMonitor(DummyVecEnv([lambda: env])))
                 for steps in train_env_steps:
                     train_env_step_list.append(steps)
-                for name in train_env_names:
-                    train_env_name_list.append(name)
+                # for name in train_env_names:
+                #     train_env_name_list.append(name)
             else:
                 train_env_list.append(VecMonitor(DummyVecEnv([lambda: env for env in train_envs])))
                 train_env_step_list.append(sum(train_env_steps))
-                train_env_name_list.append(train_env_names)
+                # train_env_name_list.append(train_env_names)
+        for each_target_config in self.target_configs:
+            target_env = CustomEnv(
+                txt_file_path=each_target_config.txt_file_path,
+                display_size=self.max_minimum_display_size,
+                display_mode=each_target_config.display_mode,
+                random_rotate=each_target_config.random_rotate,
+                random_flip=each_target_config.random_flip,
+                custom_mission=each_target_config.custom_mission,
+                max_steps=each_target_config.max_steps,
+            )
+            target_env_list.append(VecMonitor(DummyVecEnv([lambda: target_env])))
+            target_env_name_list.append(each_target_config.name)
 
         # prepare workspace and log writer
         os.makedirs(session_dir, exist_ok=True)
         log_dir = os.path.join(session_dir, "logs")
         os.makedirs(log_dir, exist_ok=True)
-        model_save_path = os.path.join(session_dir, "saved_models")
-        os.makedirs(model_save_path, exist_ok=True)
+        model_save_dir = os.path.join(session_dir, "saved_models")
+        os.makedirs(model_save_dir, exist_ok=True)
         log_writer = SummaryWriter(log_dir)
 
         # start training
+        for i in range(len(train_env_list)):
+            print(f"Training stage [{i+1} / {len(train_env_list)}]:")
+            # make call back function for testing.
+            target_callback = EvalCallback(
+                eval_envs=target_env_list,
+                eval_env_names=target_env_name_list,
+                log_writer=log_writer,
+                eval_freq=eval_freq,
+                n_eval_episodes=num_eval_episodes,
+                deterministic=eval_deterministic,
+                verbose=1,
+                start_timestep=start_time_step,
+            )
+            eval_callback = EvalSaveCallback(
+                eval_envs=eval_env_list,
+                eval_env_names=eval_env_name_list,
+                model_save_dir=model_save_dir,
+                model_save_name=f"task_{i+1}",
+                log_writer=log_writer,
+                eval_freq=eval_freq,
+                n_eval_episodes=num_eval_episodes,
+                deterministic=eval_deterministic,
+                verbose=1,
+                start_timestep=start_time_step,
+            )
+
+            callback_list = CallbackList(callbacks=[target_callback, eval_callback])
+
+            # get environment for this round
+            env = train_env_list[i]
+
+            # Load or create a new model
+            best_path = os.path.join(model_save_dir, f"task_{i}_best.zip")
+            if os.path.exists(best_path):
+                model = PPO.load(best_path, env=env)
+                print("Loaded model from saved path.")
+            else:
+                model = PPO("MlpPolicy", env, verbose=1)
+                print("Initialized new model.")
+            pass
+
+            # train
+            model.learn(total_timesteps=train_env_step_list[i], callback=callback_list, progress_bar=True)
+
+            # accumulated timestep
+            start_time_step = eval_callback.num_timesteps
 
         # close the writer
         log_writer.close()

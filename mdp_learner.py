b980d95fe899499a9e058d8f4d321c3bb5cf9537
Benteng Ma <1376937399@qq.com>
2024-08-09 01:52:02 +0100

    Remove deprecated CustomEnvFromFile class
    Deleted the entire CustomEnvFromFile class which included methods for initializing and generating grid layouts from text files. This class was replaced by an updated implementation elsewhere in the code.

--- mdp_learner.py
+++ mdp_learner.py
@@ -1,6 +1,8 @@
 from customize_minigrid.wrappers import FullyObsSB3MLPWrapper
 
 
+# decimal_number = int(np.dot(final_obs, np.power(2, np.arange(final_obs.size)[::-1])))
+
 class OneHotEncodingMDPLearner:
     def __init__(self, onehot_env: FullyObsSB3MLPWrapper):
         self.env = onehot_env

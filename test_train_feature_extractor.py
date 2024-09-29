e1e0717b86e624871c5915717331cd4e50ae73f7
Benteng Ma <1376937399@qq.com>
2024-09-29 20:09:12 +0100

    Switch to CNN encoder and update training logic
    Replaced Transformer encoder with CNN encoder in the policy. Changed environment to use FullyObsImageWrapper and AsyncVectorEnv. Updated dataset resampling and training logic to handle truncated episodes and adjusted learning rates and dataset sizes.

--- test_train_feature_extractor.py
+++ test_train_feature_extractor.py
@@ -1,17 +1,16 @@
 import os
-
-import gymnasium as gym
 import torch
-from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
 from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from customPPO import CustomPPO, CustomActorCriticPolicy, TransformerEncoderExtractor
+from customPPO import CustomPPO, CustomActorCriticPolicy, TransformerEncoderExtractor, CNNEncoderExtractor
 from customize_minigrid.custom_env import CustomEnv
-from customize_minigrid.wrappers import FullyObsSB3MLPWrapper
+from customize_minigrid.wrappers import FullyObsSB3MLPWrapper, FullyObsImageWrapper
+from easy_models import SimpleCNN
 from train import TaskConfig
+from gymnasium.vector import AsyncVectorEnv
 
 
 class GymDataset(Dataset):
@@ -22,7 +21,7 @@ class GymDataset(Dataset):
         self.data = [{}]
 
         # Create the vectorized environment with monitoring
-        self.env = VecMonitor(SubprocVecEnv([self.env_make_func for _ in range(num_envs)]))
+        self.env = AsyncVectorEnv([self.env_make_func for _ in range(num_envs)])
 
         # Initial sampling to populate the dataset
         self.resample()
@@ -30,29 +29,31 @@ class GymDataset(Dataset):
     def resample(self):
         """Resample the data by interacting with the environment and collecting a new epoch of data."""
         self.data.clear()  # Clear existing data
-        obs = self.env.reset()  # Reset the environment to get the initial observations
+        obs, infos = self.env.reset()  # Reset the environment to get the initial observations
 
         # Collect data for one full epoch with a progress bar
         for _ in tqdm(range(self.epoch_size // self.num_envs), desc="Sampling Data", unit="step"):
             # Sample actions for each parallel environment
             actions = [self.env.action_space.sample() for _ in range(self.num_envs)]
-            next_obs, rewards, dones, infos = self.env.step(actions)
+            next_obs, rewards, dones, truncated, infos = self.env.step(actions)
 
-            # Adjust the `dones` array to handle truncated episodes
-            corrected_dones = dones.copy()
-            for idx, done in enumerate(dones):
-                # If the episode was truncated, mark `done` as False to avoid incorrect terminal state handling
-                if done and infos[idx].get("TimeLimit.truncated", False):
-                    corrected_dones[idx] = False
+            # Create a variable to handle the correct 'next_obs'
+            final_next_obs = next_obs.copy()  # Copy `next_obs` to avoid modifying the original
+
+            # Replace values in `final_next_obs` with `final_observation` if done
+            if "final_observation" in infos:
+                for env_idx in range(self.num_envs):
+                    if dones[env_idx]:
+                        final_next_obs[env_idx] = infos["final_observation"][env_idx]
 
             # Store the data for each parallel environment
             for env_idx in range(self.num_envs):
                 self.data.append({
                     'obs': torch.tensor(obs[env_idx], dtype=torch.float32),
                     'action': torch.tensor(actions[env_idx], dtype=torch.int64),
-                    'next_obs': torch.tensor(next_obs[env_idx], dtype=torch.float32),
+                    'next_obs': torch.tensor(final_next_obs[env_idx], dtype=torch.float32),
                     'reward': torch.tensor(rewards[env_idx], dtype=torch.float32),
-                    'done': torch.tensor(corrected_dones[env_idx], dtype=torch.bool)
+                    'done': torch.tensor(dones[env_idx], dtype=torch.bool)
                 })
 
             obs = next_obs  # Update `obs` to `next_obs` for the next step
@@ -66,7 +67,7 @@ class GymDataset(Dataset):
 
 
 def make_env():
-    return FullyObsSB3MLPWrapper(
+    return FullyObsImageWrapper(
         CustomEnv(
             txt_file_path=None,
             rand_gen_shape=(4, 4),
@@ -83,11 +84,14 @@ def make_env():
 if __name__ == '__main__':
     session_dir = "experiments/feature_extractor_test"
     policy_kwargs = dict(
-        features_extractor_class=TransformerEncoderExtractor,  # Use the custom encoder extractor
+        features_extractor_class=CNNEncoderExtractor,  # Use the custom encoder extractor
         features_extractor_kwargs=dict(
             net_arch=[32],  # Custom layer sizes
-            num_transformer_layers=1,
-            n_heads=8,
+            cnn_net_arch=[
+                (32, 3, 2, 1),
+                (64, 3, 2, 1),
+                (128, 3, 2, 1),
+            ],
             activation_fn=nn.LeakyReLU  # Activation function
         ),
         net_arch=dict(pi=[32, 128, 128], vf=[32, 128, 128]),  # Policy and value network architecture
@@ -99,64 +103,106 @@ if __name__ == '__main__':
     # model_save_dir = os.path.join(session_dir, "saved_models")
     # os.makedirs(model_save_dir, exist_ok=True)
     log_writer = SummaryWriter(log_dir)
-    model = CustomPPO(CustomActorCriticPolicy, env=make_env(), policy_kwargs=policy_kwargs, verbose=1, log_dir=log_dir,
-                      batch_size=4)
+
+    model = CustomPPO(CustomActorCriticPolicy, env=make_env(), policy_kwargs=policy_kwargs, verbose=1, log_dir=log_dir)
     feature_model = model.policy.features_extractor
     feature_model = feature_model.cuda()
 
-    feature_model.weights = {'total': 1.0, 'inv': 1.0, 'dis': 0.0, 'neighbour': 0.1, 'dec': 0.0, 'rwd': 0.1, 'terminate': 1.0}
+    feature_model.weights = {'total': 1.0, 'inv': 0.0, 'dis': 0.0, 'neighbour': 0.0, 'dec': 0.0, 'rwd': 0.0, 'terminate': 1.0}
 
     # Create the dataset using the provided `make_env` function
-    dataset = GymDataset(make_env, int(1e5), num_envs=12)
+    dataset = GymDataset(make_env, int(1e4), num_envs=1)
     dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
 
-    optimizer = optim.Adam(feature_model.parameters(), lr=1e-4)
+    optimizer = optim.Adam(feature_model.parameters(), lr=5e-4)
 
     counter = 0
 
-    # Training loop with the initial data, with progress bar
-    for obs, actions, next_obs, rewards, dones in tqdm(dataloader, desc="Training", unit="batch"):
-        # Perform training here
-        differences = obs - next_obs
-        norms = torch.norm(differences, p=2, dim=1)
-        same_states = norms < 0.5
+    for k in range(100):
+        # Training loop with the initial data, with progress bar
+        for obs, actions, next_obs, rewards, dones in tqdm(dataloader, desc="Training", unit="batch"):
+            # Perform training here
 
-        obs = obs.cuda()
-        actions = actions.cuda()
-        next_obs = next_obs.cuda()
-        rewards = rewards.cuda()
-        dones = dones.cuda()
+            # # visualize the first example in the batch
+            # batch_idx = 0
+            # # Extract data for visualization
+            # obs_image = obs[batch_idx].numpy().transpose(1, 2, 0)  # Convert to (height, width, channels)
+            # next_obs_image = next_obs[batch_idx].numpy().transpose(1, 2, 0)  # Convert to (height, width, channels)
+            # action = actions[batch_idx].item()
+            # reward = rewards[batch_idx].item()
+            # done = dones[batch_idx].item()
+            #
+            # if done:
+            #     # Plotting
+            #     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+            #
+            #     # Display the current observation
+            #     axes[0].imshow(obs_image)
+            #     axes[0].set_title("Current Observation")
+            #     axes[0].axis('off')
+            #
+            #     # Display the next observation
+            #     axes[1].imshow(next_obs_image)
+            #     axes[1].set_title("Next Observation")
+            #     axes[1].axis('off')
+            #
+            #     # Display the action, reward, and done status
+            #     action_names = ["left", "right", "forward", "pickup", "drop", "toggle", "done"]
+            #     info_text = f"Action: {action_names[action]}\nReward: {reward}\nDone: {done}"
+            #     axes[2].text(0.5, 0.5, info_text, fontsize=14, ha='center', va='center')
+            #     axes[2].set_title("Action/Reward/Done")
+            #     axes[2].axis('off')
+            #
+            #     plt.tight_layout()
+            #     plt.show()
 
-        z0 = feature_model(obs)
-        z1 = feature_model(next_obs)
+            # Compute the differences
+            differences = obs - next_obs
+            # Flatten the differences along all non-batch dimensions
+            flattened_differences = differences.flatten(start_dim=1)
+            # Compute the L2 norm along the flattened dimension
+            norms = torch.norm(flattened_differences, p=2, dim=1)
+            # Compare with the threshold to obtain a boolean tensor
+            same_states = norms < 0.5
 
-        # filter out the cases that the agent did not move
-        z0_filtered = z0[~same_states]
-        z1_filtered = z1[~same_states]
+            obs = obs.cuda()
+            actions = actions.cuda()
+            next_obs = next_obs.cuda()
+            rewards = rewards.cuda()
+            dones = dones.cuda()
 
-        # get fake z1
-        idx = torch.randperm(len(obs))
-        fake_z1 = z1.view(len(z1), -1)[idx].view(z1.size())
-        fake_z1_filtered = fake_z1[~same_states]
+            z0 = feature_model(obs)
+            z1 = feature_model(next_obs)
 
-        actions_filtered = actions[~same_states]
+            # filter out the cases that the agent did not move
+            z0_filtered = z0[~same_states]
+            z1_filtered = z1[~same_states]
 
-        try:
-            loss, loss_vals = feature_model.compute_loss(
-                obs, next_obs, z0, z1, z0_filtered, z1_filtered,
-                fake_z1_filtered, actions, actions_filtered, rewards,
-                dones,
-            )
+            # get fake z1
+            idx = torch.randperm(len(obs))
+            fake_z1 = z1.view(len(z1), -1)[idx].view(z1.size())
+            fake_z1_filtered = fake_z1[~same_states]
 
-            names = ['feature_loss', 'rec_loss', 'inv_loss', 'ratio_loss', 'reward_loss', 'terminate_loss',
-                     'neighbour_loss']
-            for name, val in zip(names, loss_vals):
-                log_writer.add_scalar(name, val, counter)
+            actions_filtered = actions[~same_states]
 
-            counter += 1
+            try:
+                loss, loss_vals = feature_model.compute_loss(
+                    obs, next_obs, z0, z1, z0_filtered, z1_filtered,
+                    fake_z1_filtered, actions, actions_filtered, rewards,
+                    dones,
+                )
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        except Exception as e:
-            print(e)
+                names = ['feature_loss', 'rec_loss', 'inv_loss', 'ratio_loss', 'reward_loss', 'terminate_loss',
+                         'neighbour_loss']
+                for name, val in zip(names, loss_vals):
+                    log_writer.add_scalar(name, val, counter)
+
+                counter += 1
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            except Exception as e:
+                print(e)
+
+

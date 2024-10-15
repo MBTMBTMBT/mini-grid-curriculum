849edfe54827556e0ad5de76bc89c77521ce7653
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-10-15 13:16:39 +0100

    Adjust training parameters in world model script
    Increased dataset repetition per epoch from 10 to 25 and reduced the number of epochs from 50 to 20. This aims to balance the training process by exposing the model to more repeated instances in fewer epochs.

--- my_little_lovely_cute_world_model.py
+++ my_little_lovely_cute_world_model.py
@@ -540,8 +540,8 @@ if __name__ == '__main__':
 
     session_dir = r"./experiments/world_model-door_key-7"
     dataset_samples = int(1e4)
-    dataset_repeat_each_epoch = 10
-    num_epochs = 50
+    dataset_repeat_each_epoch = 25
+    num_epochs = 20
     batch_size = 32
     lr = 1e-4
     discriminator_lr=5e-5

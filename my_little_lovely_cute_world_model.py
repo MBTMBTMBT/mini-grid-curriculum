Fix slicing issue in predicted next state calculation
Adjusted the slicing in the torch.cat call to include all remaining channels. This fix ensures that the predicted next state is correctly formed by concatenating the predicted homomorphism latent state with the rest of the latent state channels.

my_little_lovely_cute_world_model.py
@@ -318,7 +318,7 @@ class WorldModel(nn.Module):
         predicted_next_homo_latent_state, mean, logvar, predicted_reward = self.transition_model(homo_latent_state, action)
 
         # Make homomorphism next state
-        predicted_next_state = torch.cat([predicted_next_homo_latent_state, latent_next_state[:, self.num_homomorphism_channels:-1, :, :]], dim=1)
+        predicted_next_state = torch.cat([predicted_next_homo_latent_state, latent_next_state[:, self.num_homomorphism_channels:, :, :]], dim=1)
 
         # Reconstruct the predicted next state
         reconstructed_state = self.decoder(predicted_next_state)
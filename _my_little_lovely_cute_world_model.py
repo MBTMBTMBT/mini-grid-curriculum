39d0c8b7cd75cdec50bc970b9b11753d33ff6cf0
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-10-23 02:49:47 +0100

    Switch to SimpleTransitionModel and update predictions.
    Replaced TransitionModelVAE with SimpleTransitionModel for predicting the next latent state and reward. Adjusted predictions to omit mean and logvar outputs accordingly for simplification.

--- _my_little_lovely_cute_world_model.py
+++ _my_little_lovely_cute_world_model.py
@@ -438,7 +438,8 @@ class WorldModel(nn.Module):
         self.homomorphism_latent_space = (num_homomorphism_channels, latent_shape[1], latent_shape[2])
         self.encoder = Encoder(obs_shape, latent_shape, cnn_net_arch)
         self.decoder = Decoder(latent_shape, obs_shape, cnn_net_arch)
-        self.transition_model = TransitionModelVAE(self.homomorphism_latent_space, num_actions, transition_model_conv_arch)
+        # self.transition_model = TransitionModelVAE(self.homomorphism_latent_space, num_actions, transition_model_conv_arch)
+        self.transition_model = SimpleTransitionModel(self.homomorphism_latent_space, num_actions, transition_model_conv_arch)
 
         self.num_actions = num_actions
 
@@ -468,7 +469,9 @@ class WorldModel(nn.Module):
 
         # Predict the next latent state and reward with the transition model
         action = F.one_hot(action, self.num_actions).type(torch.float)
-        predicted_next_homo_latent_state, mean, logvar, predicted_reward, predicted_done \
+        # predicted_next_homo_latent_state, mean, logvar, predicted_reward, predicted_done \
+        #     = self.transition_model(homo_latent_state, action)
+        predicted_next_homo_latent_state, predicted_reward, predicted_done \
             = self.transition_model(homo_latent_state, action)
 
         # Make homomorphism next state
@@ -540,7 +543,9 @@ class WorldModel(nn.Module):
 
         # Predict the next latent state and reward with the transition model
         action = F.one_hot(action, self.num_actions).type(torch.float)
-        predicted_next_homo_latent_state, mean, logvar, predicted_reward, predicted_done \
+        # predicted_next_homo_latent_state, mean, logvar, predicted_reward, predicted_done \
+        #     = self.transition_model(homo_latent_state, action)
+        predicted_next_homo_latent_state, predicted_reward, predicted_done \
             = self.transition_model(homo_latent_state, action)
 
         # Make homomorphism next state

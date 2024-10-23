225dde13124864ed001f87cfbd281e792d992ec0
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-10-23 17:25:20 +0100

    Scale KL divergence loss by 0.1
    Multiplying the KL divergence loss by 0.1 in the VAE model to adjust its contribution to the overall loss calculation. This change aims to fine-tune the balance between reconstruction and regularization in the latent space.

--- _my_little_lovely_cute_world_model.py
+++ _my_little_lovely_cute_world_model.py
@@ -600,6 +600,7 @@ class WorldModel(nn.Module):
         # latent_transition_loss = latent_transition_loss_mse  # + latent_transition_loss_mae
 
         kl_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())  # * 0.1
+        kl_loss *= 0.1
         # vae_loss = latent_transition_loss + 0.1 * kl_loss
 
         # --------------------
